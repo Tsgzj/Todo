4c0e588754ed3a0eb86e6aff8068bfd6be0ceaff
Tsgzj <wxsun1991@gmail.com>
2020-08-16 15:08:09 -0700

    Fixed footer

--- components/Main.js
+++ components/Main.js
@@ -24,9 +24,9 @@ function Copyright() {
     return (
             <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
+            <Link color="inherit" href="https://tsgzj.me">
             Tsgzj
-        </Link>{'https://tsgzj.me'}
+        </Link>
         {new Date().getFullYear()}
         {'.'}
         </Typography>
